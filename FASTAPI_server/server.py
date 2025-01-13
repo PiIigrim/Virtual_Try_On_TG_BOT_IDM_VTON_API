fdedb3666c2e1d8896fce49a01a534bf6ba44189
Piligrim <nikita.romanko.2004@gmail.com>
2025-01-13 14:32:37 +0300

    move crop code to own file

--- FASTAPI_server/server.py
+++ FASTAPI_server/server.py
@@ -9,9 +9,7 @@ import asyncio
 import logging
 from config import Config
 import base64
-import cv2
-from ultralytics import YOLO
-import mediapipe as mp
+from img_utils import crop_person
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__)) 
 UPLOAD_DIR = os.path.join(BASE_DIR, 'uploads')
@@ -26,90 +24,6 @@ setup_logger()
 
 gradio_client = Client(config.model_name, hf_token=config.model_name)
 
-def is_full_body(image_path):
-    mp_pose = mp.solutions.pose
-    pose = mp_pose.Pose(static_image_mode=True, model_complexity=2)
-
-    image = cv2.imread(image_path)
-    if image is None:
-        print(f"Error: Could not read image at {image_path}")
-        return False
-
-    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    results = pose.process(image_rgb)
-
-    if not results.pose_landmarks:
-        print("No person detected in the image.")
-        return False
-
-    landmarks = results.pose_landmarks.landmark
-
-    try:
-        left_ankle_visible = landmarks[mp_pose.PoseLandmark.LEFT_ANKLE].visibility > 0.5
-        right_ankle_visible = landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE].visibility > 0.5
-    except Exception as e:
-        print(f"Error getting landmarks: {e}")
-        return False
-    
-    pose.close()
-
-    if left_ankle_visible or right_ankle_visible:
-        return True
-    else:
-        return False
-    
-def crop_person(image_path, crop_ratio=0.2):
-    try:
-        model = YOLO("yolo11n.pt")
-    except Exception as e:
-        print(f"Error loading model: {e}")
-        return None
-
-    image = cv2.imread(image_path)
-    if image is None:
-        print(f"Error: Could not read image at {image_path}")
-        return None
-
-    results = model(image)
-
-    persons = []
-    for result in results:
-        if result.boxes is not None:
-            for box in result.boxes:
-                cls = int(box.cls)
-                if cls == 0:
-                    x1, y1, x2, y2 = map(int, box.xyxy[0])
-                    persons.append((x1, y1, x2, y2))
-
-    if not persons:
-        print("No person detected in the image.")
-        return None
-
-    max_area = 0
-    max_person_bbox = None
-    for x1, y1, x2, y2 in persons:
-        area = (x2 - x1) * (y2 - y1)
-        if area > max_area:
-            max_area = area
-            max_person_bbox = (x1, y1, x2, y2)
-
-    if max_person_bbox:
-        x1, y1, x2, y2 = max_person_bbox
-        height = y2 - y1
-        
-        full_body = is_full_body(image_path)
-
-        if not full_body:
-            crop_y2 = int(y2 + (y2 - y1) * crop_ratio)
-            crop_y2 = min(crop_y2, image.shape[0])
-            cropped_image = image[y1:crop_y2, x1:x2]
-        else:
-            knee_level = int(y1 + height * 0.65)
-            cropped_image = image[y1:knee_level, x1:x2]
-            
-        return cropped_image
-    else:
-        return None
 
 @app.get("/")
 async def read_root():
@@ -171,13 +85,11 @@ async def process_files(task_id: str, user_photo_path: str, product_image_path: 
             processing_results[task_id] = {'status': 'error', 'message': f"File {product_image_path} not found."}
             return
 
-        cropped_image = crop_person(user_photo_path)
-        cv2.imwrite(user_photo_path, cropped_image)
-
         with open(config.js_data_url, 'r', encoding='utf-8') as f:
           products = json.load(f)
           product_id = products[current_index]['cloth_type']
           if product_id == "Верх":
+            crop_person(user_photo_path, base_crop=False)
             result_gradio = await asyncio.to_thread(gradio_client.predict,
 		          src_image_path=handle_file(user_photo_path),
 		          ref_image_path=handle_file(product_image_path),
@@ -190,6 +102,7 @@ async def process_files(task_id: str, user_photo_path: str, product_image_path: 
 		          api_name="/leffa_predict_vt"
             )
           elif product_id == "Низ":
+            crop_person(user_photo_path, base_crop=True)
             result_gradio = await asyncio.to_thread(gradio_client.predict,
 		          src_image_path=handle_file(user_photo_path),
 		          ref_image_path=handle_file(product_image_path),
